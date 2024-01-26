d89e4bccf95b888519677b1ec045066c609926f2
YuriyG <mym21072011@gmail.com>
2024-01-26 19:24:00 +0200

    LFE-0004 Navagation graph was added

---
+++ common/umbrella/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.jetbrainsCompose)
+}
+
+kotlin {
+
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = JavaVersion.VERSION_17.toString()
+            }
+        }
+    }
+
+    jvm("desktop")
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+
+            implementation(projects.common.core)
+            implementation(projects.common.splash.compose)
+
+            implementation(libs.odyssey.core)
+            implementation(libs.odyssey.compose)
+        }
+    }
+}
+
+android {
+    namespace = "com.life4earth.app.umbrella"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        jvmToolchain {
+            languageVersion.set(JavaLanguageVersion.of(JavaVersion.VERSION_17.toString()))
+        }
+    }
+}
---
+++ common/umbrella/src/commonMain/kotlin/di/PlatformSDK.kt
@@ -0,0 +1,26 @@
+package di
+
+import coreModule
+import org.kodein.di.DI
+import org.kodein.di.bind
+import org.kodein.di.direct
+import org.kodein.di.singleton
+
+object PlatformSDK {
+
+    fun init(configuration: PlatformConfiguration) {
+        val umbrellaModule = DI.Module(
+            name = "umbrellaModule",
+            init = { bind<PlatformConfiguration>() with singleton { configuration } }
+        )
+
+        Inject.createDependencies(
+            DI {
+                importAll(
+                    coreModule,
+                    umbrellaModule
+                )
+            }.direct
+        )
+    }
+}
