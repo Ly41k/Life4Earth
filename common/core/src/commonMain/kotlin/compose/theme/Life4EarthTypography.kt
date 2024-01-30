37410ace09757cf02a3e1d73286c6db70a2cadde
YuriyG <mym21072011@gmail.com>
2024-01-30 14:09:51 +0200

    LFE-0006 Add app theme

---
+++ common/core/src/commonMain/kotlin/compose/theme/Life4EarthTypography.kt
@@ -0,0 +1,11 @@
+package compose.theme
+
+import androidx.compose.ui.text.TextStyle
+
+data class Life4EarthTypography(
+    val largeHeading: TextStyle,
+    val mediumHeading: TextStyle,
+    val smallHeading: TextStyle,
+    val text: TextStyle,
+    val label: TextStyle
+)
