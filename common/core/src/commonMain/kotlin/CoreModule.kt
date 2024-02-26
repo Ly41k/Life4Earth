e398f3244ef5b415f1da121e18bd34903c714a06
YuriyG <mym21072011@gmail.com>
2024-02-26 15:37:01 +0200

    LFE-0010 Update DI

--- common/core/src/commonMain/kotlin/CoreModule.kt
+++ common/core/src/commonMain/kotlin/CoreModule.kt
@@ -1,8 +1,6 @@
 import ktor.ktorModule
-import org.kodein.di.DI
+import org.koin.dsl.module
 
-val coreModule = DI.Module("coreModule") {
-    importAll(
-        ktorModule
-    )
+val coreModule = module {
+    includes(ktorModule)
 }
