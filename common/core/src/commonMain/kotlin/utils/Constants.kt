fe9ea73169d32b4b9c6bc592b11e5ed0258d1197
YuriyG <mym21072011@gmail.com>
2024-02-27 13:21:56 +0200

    LFE-0012 Added dynamic change of application icon when changing the dark mode

--- common/core/src/commonMain/kotlin/utils/Constants.kt
+++ common/core/src/commonMain/kotlin/utils/Constants.kt
@@ -2,4 +2,7 @@ package utils
 
 object Constants {
     const val BASE_URL: String = "https://google.com"
+
+    const val ACTION_DARK_MODE_ICON = "com.life4earth.app.ACTION_DARK_MODE_ICON"
+    const val ACTION_LIGHT_MODE_ICON = "com.life4earth.app.ACTION_LIGHT_MODE_ICON"
 }
